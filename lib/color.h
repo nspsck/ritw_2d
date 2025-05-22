71e708687336439213e14d55b8a1e73a0672ba99
nspsck <nspsck@nspsck.com>
2025-05-22 09:25:11 +0200

    feat(lines): hline and vline

--- lib/color.h
+++ lib/color.h
@@ -8,6 +8,10 @@
 
 #define color_swap_bytes(c) (((c) >> 8) | ((c) << 8))
 
+/**
+ * Convert r,g,b to color565 ready for the renderer to draw. Should be only used
+ * for drawing functions.
+ */
 uint16_t rgb_to_swapped_565(uint8_t r, uint8_t g, uint8_t b);
 
 #endif // COLOR_H
